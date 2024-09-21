f7cc198eef88b516bfe655394d42d85616b6b0c1
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-09-21 22:12:14 +1000

    added custom button (Button73)

---
+++ src/components/Button73.tsx
@@ -0,0 +1,22 @@
+type Button73Props = {
+  handleClickCallback: () => void;
+  isActive?: boolean;
+};
+
+const Button73: React.FC<Button73Props> = (props) => {
+  const { handleClickCallback, isActive = false } = props;
+
+  return (
+    <button
+      className={`button-73 bg-white rounded-md border-none shadow-inner inline-block text-black text-xs font-sans font-medium cursor-pointer leading-none m-0 outline-none px-2 py-1 transition-all duration-150 ease-in-out hover:bg-yellow-400 hover:shadow-inner-orange-500 hover:scale-105 ${
+        isActive ? "bg-yellow-400 -inner-orange-500 scale-105" : ""
+      }`}
+      type="button"
+      role="button"
+    >
+      PRESS ME PLEASE
+    </button>
+  );
+};
+
+export default Button73;
