65a0da4c9efb62a0113eb54cfa44e38ba8f9a2fc
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-09-25 23:36:42 +1000

    invert scale on hover behaviour

--- src/components/Button73.tsx
+++ src/components/Button73.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 type Button73Props = {
   handleClick: () => void;
-  isDisableHoverScale?: boolean;
+  isScaleOnHover?: boolean;
   text?: string;
   children?: React.ReactNode;
 };
 
 const Button73 = React.forwardRef<HTMLButtonElement, Button73Props>((props, ref) => {
-  const { handleClick, isDisableHoverScale = false, text, children, ...rest } = props;
+  const { handleClick, isScaleOnHover = false, text, children, ...rest } = props;
 
   return (
     <button
@@ -16,7 +16,7 @@ const Button73 = React.forwardRef<HTMLButtonElement, Button73Props>((props, ref)
       {...rest} // for <Tooltip> wrapper
       onClick={handleClick}
       className={`button-73 bg-white rounded-md border-none shadow-inner inline-block text-black text-xs font-sans font-medium cursor-pointer leading-none m-0 outline-none px-2 py-1 transition-all duration-150 ease-in-out hover:bg-yellow-400 hover:shadow-inner-orange-500 ${
-        isDisableHoverScale ? "" : "hover:scale-105"
+        isScaleOnHover ? "hover:scale-105" : ""
       }`}
       type="button"
       role="button"
