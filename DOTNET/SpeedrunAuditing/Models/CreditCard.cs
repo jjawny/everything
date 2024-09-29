586877838a0068ad243e63c512a9273c15c83160
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-09-29 17:34:01 +1000

    make credit card fields optional for demo

--- DOTNET/SpeedrunAuditing/Models/CreditCard.cs
+++ DOTNET/SpeedrunAuditing/Models/CreditCard.cs
@@ -3,10 +3,10 @@ namespace SpeedrunAuditing.Models;
 public class CreditCard
 {
   public Guid Id { get; set; }
-  public string Name { get; set; } = null!;
-  public string Bank { get; set; } = null!;
-  public int Number { get; set; }
-  public DateTime Expiry { get; set; }
-  public int CVV { get; set; }
+  public string? Name { get; set; }
+  public string? Bank { get; set; }
+  public int? Number { get; set; }
+  public DateTime? Expiry { get; set; }
+  public int? Cvc { get; set; }
   public Audit Audit { get; set; } = new();
 }
