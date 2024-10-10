684cd94c7517d84d4fd1b4c284218fb925b2f755
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-10-10 23:59:22 +1000

    style for header

--- src/components/shared/UnderConstructionHeader.tsx
+++ src/components/shared/UnderConstructionHeader.tsx
@@ -1,8 +1,8 @@
 const UnderConstructionHeader: React.FC = () => {
   return (
-    <header className="fixed w-screen flex justify-center flex-col items-center">
+    <header className="z-50 fixed select-none top-0 left-0 font-mono w-screen opacity-60 flex justify-center flex-col items-center">
       <div className="bg-amber-300 w-full h-1"></div>
-      <div className="bg-amber-300 rounded-b-md pb-1 px-3 w-fit text-xs max-h-0 hover:max-h-96 text-amber-900 ease-in-out duration-300 transition-max-height overflow-hidden">
+      <div className="bg-amber-300 rounded-b-md pb-1 px-3 w-fit text-xs border-b-[1px] border-x-[1px] border-amber-200 text-amber-900">
         🚧 this is a sandbox 🚧
       </div>
     </header>
