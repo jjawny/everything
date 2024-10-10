f41aac455a41b51169fb0b5e78512392be30397f
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-10-11 00:32:04 +1000

    use under construction bg

--- src/components/shared/UnderConstructionHeader.tsx
+++ src/components/shared/UnderConstructionHeader.tsx
@@ -8,12 +8,12 @@ const UnderConstructionHeader: React.FC = () => {
 
   return (
     <header
-      className={`z-50 fixed select-none top-0 left-0 font-sedgwick w-screen transition-all ease-in-out duration-500 flex flex-col items-center
+      className={`z-50 fixed select-none top-0 left-0 font-sedgwick w-screen transition-all ease-in-out duration-200 flex flex-col items-center
     ${isHover ? "opacity-80" : "opacity-40"}`}
     >
-      <div className="bg-amber-300 w-full h-1"></div>
+      <div className="bg-amber-300 bg-under-construction w-full h-1"></div>
       <div
-        className="bg-amber-300 rounded-b-md pb-1 px-2 w-fit text-xs hover:text-sm transition-all ease-in-out duration-500 border-b-[1px] border-x-[1px] border-amber-200 text-amber-900"
+        className="bg-amber-300 bg-under-construction pr-3 rounded-b-md pb-1 px-2 w-fit text-xs hover:scale-x-105 transition-all ease-in-out duration-100 border-b-[1px] border-x-[1px] border-amber-200 text-amber-900"
         onMouseEnter={() => setIsHover(true)}
         onMouseLeave={() => setIsHover(false)}
       >
