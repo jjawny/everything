9fd589258b611b7362784ecc791bfe9c23c78606
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-10-11 00:27:07 +1000

    only trigger hover styles over tear drop, not entire (full width) bar

--- src/components/shared/UnderConstructionHeader.tsx
+++ src/components/shared/UnderConstructionHeader.tsx
@@ -1,9 +1,23 @@
+"use client";
+
+import InfoIcon from "@mui/icons-material/Info";
+import { useState } from "react";
+
 const UnderConstructionHeader: React.FC = () => {
+  const [isHover, setIsHover] = useState<boolean>();
+
   return (
-    <header className="z-50 fixed select-none top-0 left-0 font-mono w-screen opacity-60 flex justify-center flex-col items-center">
+    <header
+      className={`z-50 fixed select-none top-0 left-0 font-sedgwick w-screen transition-all ease-in-out duration-500 flex flex-col items-center
+    ${isHover ? "opacity-80" : "opacity-40"}`}
+    >
       <div className="bg-amber-300 w-full h-1"></div>
-      <div className="bg-amber-300 rounded-b-md pb-1 px-3 w-fit text-xs border-b-[1px] border-x-[1px] border-amber-200 text-amber-900">
-        🚧 this is a sandbox 🚧
+      <div
+        className="bg-amber-300 rounded-b-md pb-1 px-2 w-fit text-xs hover:text-sm transition-all ease-in-out duration-500 border-b-[1px] border-x-[1px] border-amber-200 text-amber-900"
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+      >
+        <InfoIcon fontSize="small" color="disabled" className="mr-1" /> this is a sandbox!!!
       </div>
     </header>
   );
